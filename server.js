e619586c13487a2b9777d4221c5ab400714923bc
shubham.ralli <shubham.ralli@socialpilot.co>
2022-10-29 00:40:01 +0530

    server change

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
 
-server.listen(9000,console.log('app is running'));
+// server.listen(9000,console.log('app is running'));
 
-// server.listen(process.env.PORT,console.log('app is running'));
+server.listen(process.env.PORT,console.log('app is running'));
